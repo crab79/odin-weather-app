d82c5e8ea1f8d4d261f45da0286906ede26d8c3e
crab79 <hanyaoyao0709@gmail.com>
2024-09-12 22:18:38 +0800

    Finished the search GIF function and fixed some bugs.

--- src/index.js
+++ src/index.js
@@ -3,75 +3,80 @@ import "./style.scss";
 import iconMap from "./icons";
 
 const button_find = document.querySelector("button#search");
+const button_gif_find = document.querySelector("button#gif_search");
 const text_find = document.querySelector("input#search");
 const button_c = document.querySelector("button#c");
 const button_f = document.querySelector("button#f");
 const loading = document.querySelector("dialog#loading");
-const container = document.querySelector("div#infos");
+const container = document.querySelector("div#infos")
+const description_general = document.querySelector("h1#general_desc");
 
 let loading_text = document.querySelector("dialog#loading h1");
 let unit = "metric";
 let weather = {};
 
-async function find_weather(place, unit) {
-  container.textContent = "";
-  loading.showModal();
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/next7days?unitGroup=${unit}&key=BF3M7227BV3YDJD9YV93QP2BM`,
-    { mode: "cors" }
-  );
-  if (!response.ok && response.status === 400) {
-    setTimeout(() => {
-      loading_text.textContent =
-        "Did not find the city. Please check your input.";
-      setTimeout(() => {
-        loading.close();
-        loading_text.textContent = "loading...";
-      }, 1000);
-    }, 0); // Display the message immediately
-    return;
+async function find_weather(place, unit) {  
+  try {
+    container.textContent = "";
+    description_general.textContent = "";
+    loading.showModal();
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/next7days?unitGroup=${unit}&key=BF3M7227BV3YDJD9YV93QP2BM`,
+      { mode: "cors" }
+    );
+
+    if (!response.ok && response.status === 400) {
+      throw new Error("Did not find the city. Please check your input.");
+    }
+
+    const datas = await response.json();
+    console.log(datas);
+
+    for (let i = 0; i < 8; i++) {
+      const weather = {
+        icon: datas.days[i].icon,
+        temp: datas.days[i].temp,
+        temp_max: datas.days[i].tempmax,
+        temp_min: datas.days[i].tempmin,
+        humidity: datas.days[i].humidity,
+        precipprob: datas.days[i].precipprob,
+        description: datas.days[i].description,
+        description_general: datas.description,
+      };
+      display_weather(weather, i, unit);
+    }
+
+  } catch (error) {
+    showLoadingMessage(error.message);
+  } finally {
+    loading.close();
+    loading_text.textContent = "loading...";
+
   }
-  const datas = await response.json();
-  console.log(datas);
-  // let description_general = datas.description;
-  for (let i = 0; i < 8; i++) {
-    // let icon = datas.days[i].icon; //text --> img
-    // let temp = datas.days[i].temp;
-    // let temp_max = datas.days[i].tempmax;
-    // let temp_min = datas.days[i].tempmin;
-    // let humidity = datas.days[i].humidity;
-    // let precipprob = datas.days[i].precipprob;
-    // let description = datas.days[i].description;
-    // console.log(icon, description, humidity, temp);
-    weather = {
-      icon: datas.days[i].icon,
-      temp: datas.days[i].temp,
-      temp_max: datas.days[i].tempmax,
-      temp_min: datas.days[i].tempmin,
-      humidity: datas.days[i].humidity,
-      precipprob: datas.days[i].precipprob,
-      description: datas.days[i].description,
-      description_general: datas.description,
-    };
-    display_weather(weather, i, unit);
-    // for (const key in weather) {
-    //     console.log(`${key} = ${weather[key]}`);
-    // }
-  }
-  loading.close();
 }
 
-// async function find_img(text) {
-//     const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=eCVJCElMiCVVVUjPJl3b0wjsAC3lb5PJ&s=${text}`, { mode: 'cors' })
-//     const datas = await response.json();
-//     console.log(datas);
-//     if (datas.data.images === undefined) {
-//       alert(datas.meta.msg);
-//       return;
-//     } else {
-//       img.src = datas.data.images.original.url;
-//     }
-//   }
+async function find_img(text) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=eCVJCElMiCVVVUjPJl3b0wjsAC3lb5PJ&s=${text}`,
+      { mode: "cors" }
+    );
+
+    const datas = await response.json();
+    console.log(datas);
+
+    if (!datas.data.images) {
+      throw new Error(datas.meta.msg);
+    }
+
+    return datas.data.images.original.url;
+
+  } catch (error) {
+    showLoadingMessage(error.message + ", please use normal search");
+    return null;
+  }
+}
+
 
 async function display_weather(obj, i, unit) {
   const info = document.createElement("div");
@@ -83,7 +88,6 @@ async function display_weather(obj, i, unit) {
   const humidity = document.createElement("h2");
   const precipprob = document.createElement("h1");
   const description = document.createElement("p");
-  const description_general = document.querySelector("h1#general_desc");
   info.className = "info";
   temp_group.className = "temp_min_max";
   temp_max.className = "temp_min_max";
@@ -94,12 +98,22 @@ async function display_weather(obj, i, unit) {
 
   if (unit == "metric") {
     unit = "C";
+  }else if(unit == "us"){
+    unit = "F";
   }
 
   for (const key in obj) {
     switch (key) {
       case "icon":
-        img.src = iconMap[obj[key]];
+        if (button_gif_find.classList.contains("click")) {
+          const imgUrl = await find_img(obj[key]);
+          if (imgUrl) {
+            // check if imgUrl is valid before assigning
+            img.src = imgUrl;
+          }
+        } else {
+          img.src = iconMap[obj[key]];
+        }
         break;
       case "temp":
         temp.textContent = `Temp: ${obj[key]}°${unit}`;
@@ -119,23 +133,11 @@ async function display_weather(obj, i, unit) {
       case "description":
         description.textContent = obj[key];
         break;
-
-      default:
-        loading.showModal();
-        setTimeout(() => {
-          loading_text.textContent =
-            "Did not find the city. Please check your input.";
-          setTimeout(() => {
-            loading.close();
-            loading_text.textContent = "loading...";
-          }, 1000);
-        }, 0);
-        break;
     }
   }
 
   display_date(i, info);
-
+  
   temp_group.append(temp_max, temp_min);
   container.append(info);
   info.append(img, temp, temp_group, humidity, precipprob, description);
@@ -150,8 +152,27 @@ function display_date(i, info) {
   info.append(date);
 }
 
+function showLoadingMessage(message) {
+  setTimeout(() => {
+    loading.showModal()
+    loading_text.textContent = message;
+    setTimeout(() => {
+      loading.close();
+      loading_text.textContent = "loading...";
+    }, 3000);
+  }, 0); }
+
 button_find.addEventListener("click", () => {
   let text = text_find.value;
+  button_gif_find.classList.remove("click");
+
+  find_weather(text, unit);
+});
+
+button_gif_find.addEventListener("click", () => {
+  let text = text_find.value;
+  button_gif_find.classList.add("click");
+
   find_weather(text, unit);
 });
 
